3cabb805b5250da438005eaa31e04caba7453f42
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-04-01 12:52:24 -0400

    added cors policy

--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Policy access definition
+//The application will allow requests from any domain to access its resources.
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", b => b.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +22,8 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("Allowall");
 
 app.UseAuthorization();
 
