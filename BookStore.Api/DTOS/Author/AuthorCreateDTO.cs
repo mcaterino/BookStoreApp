dd32474ad53ee01ad6369ab97c457b119150206c
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-04-04 09:23:48 -0400

    added author dto

---
+++ BookStore.Api/DTOS/Author/AuthorCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.DTOS.Author
+{
+    public class AuthorCreateDTO    
+    {
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string? Bio { get; set; }
+    }
+}
