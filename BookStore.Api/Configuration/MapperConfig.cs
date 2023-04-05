d7811b9e5d1c74ae55844b85984fa0b664712a2d
Mauricio Caterino <mauricio_caterino@outlook.com>
2023-04-05 16:49:11 -0400

    updated mapper config

--- BookStore.Api/Configuration/MapperConfig.cs
+++ BookStore.Api/Configuration/MapperConfig.cs
@@ -9,10 +9,23 @@ namespace BookStore.Api.Configuration
     {
         public MapperConfig()
         {
+            CreateMap<AuthorReadOnlyDTO, Author>().ReverseMap();
             CreateMap<AuthorCreateDTO, Author>().ReverseMap();
             CreateMap<AuthorUpdateDTO, Author>().ReverseMap();
-            CreateMap<AuthorReadOnlyDTO, Author>().ReverseMap();
             CreateMap<BookCreateDTO, Book>().ReverseMap();
+            CreateMap<BookUpdateDTO, Book>().ReverseMap();
+
+            //Mapping from Book to get author's name
+            CreateMap<Book, BookReadOnlyDTO>()
+                .ForMember(a => a.AuthorName, 
+                           b => b.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
+            //Mapping from Book to get author's name
+            CreateMap<Book, BookDetailsDTO>()
+                .ForMember(a => a.AuthorName,
+                           b => b.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
         }
     }
 }
