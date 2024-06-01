ab887aa51e942e25d322f4203b3498a0d4e0585d
Mauricio Caterino <mauricio_caterino@outlook.com>
2024-06-01 11:25:40 -0400

    Added a Publisher Controller

---
+++ BookStore.Api/Controllers/PublishersController.cs
@@ -0,0 +1,139 @@
+﻿using AutoMapper;
+using BookStore.Api.Data;
+using BookStore.Api.DTOS.Publisher;
+using BookStore.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private readonly AppDbContext? _context;
+        private readonly IMapper? _mapper;
+
+        public PublishersController(AppDbContext? context, IMapper? mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/Publishers
+        [HttpGet]
+        public IActionResult GetPublishers()
+        {
+            if (_context?.Publisher is null)
+            {
+                return NotFound();
+            }
+
+            var publishers = _context.Publisher;
+            var publisherDtos = _mapper?.Map<IEnumerable<PublisherReadOnlyDTO>>(publishers);
+            return Ok(publisherDtos);
+        }
+
+        // GET: api/Publishers/5
+        [HttpGet("{id}")]
+        public IActionResult GetPublisher(int id)
+        {
+            if (_context?.Publisher is null)
+            {
+                return NotFound();
+            }
+
+            var publisher = _context.Publisher.Find(id);
+
+            if (publisher is null)
+            {
+                return NotFound();
+            }
+
+            var publisherDto = _mapper?.Map<PublisherReadOnlyDTO>(publisher);
+            return Ok(publisherDto);
+        }
+
+        // POST: api/Publishers/
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PublisherCreateDTO>> PostPublisher(PublisherCreateDTO publisherDto)
+        {
+            if (_context?.Publisher is null)
+            {
+                return Problem("Entity set 'AppDbContext.Publisher'  is null.");
+            }
+
+            var publisher = _mapper?.Map<Publisher>(publisherDto);
+            if (publisher == null)
+            {
+                return BadRequest("Unable to map the provided publisherDto to a Publisher entity.");
+            }
+
+            _context.Publisher.Add(publisher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPublisher", new { id = publisher.Id }, publisher);
+        }
+
+        // PUT: api/Publisher/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublisher(int id, PublisherUpdateDTO publisherDto)
+        {
+            if (id != publisherDto.Id)
+            {
+                return BadRequest();
+            }
+
+            var publisher = await _context.Publisher.FindAsync(id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(publisherDto, publisher);
+            _context.Entry(publisher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Publisher/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublisher(int id)
+        {
+            var publisher = await _context.Publisher.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Publisher.Remove(publisher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return (_context.Publisher?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
