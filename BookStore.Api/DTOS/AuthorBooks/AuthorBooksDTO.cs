aadfa04ef638b1ca36b535923c7d8b4b72f6c52c
Mauricio Caterino <mauricio_caterino@outlook.com>
2024-06-03 11:07:16 -0400

    Corrected namespace

--- BookStore.Api/DTOS/AuthorBooks/AuthorBooksDTO.cs
+++ BookStore.Api/DTOS/AuthorBooks/AuthorBooksDTO.cs
@@ -1,8 +1,8 @@
-namespace BookStore.Api.DTOS.Publisher
+namespace BookStore.Api.DTOS.AuthorBooks
 {
     public class AuthorBooksDTO : BaseDto
     {
-         public int AuthorId { get; set; }
+        public int AuthorId { get; set; }
         public string? AuthorName { get; set; }
         public int BookId { get; set; }
         public string? BookTitle { get; set; }
